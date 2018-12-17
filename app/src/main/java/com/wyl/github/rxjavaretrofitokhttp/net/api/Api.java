69deedb95bc25332a9879afc5aa246f10d64f3e7
kmf <kmf>
2018-12-17 18:23:28 +0800

    add test url

--- app/src/main/java/com/wyl/github/rxjavaretrofitokhttp/net/api/Api.java
+++ app/src/main/java/com/wyl/github/rxjavaretrofitokhttp/net/api/Api.java
@@ -1,5 +1,13 @@
 package com.wyl.github.rxjavaretrofitokhttp.net.api;
 
 public interface Api {
+    //玩Android开放API
 
+    public String baseUrl = "http://www.wanandroid.com/";//baseRul
+
+    public String chaptersUrl = "wxarticle/chapters/json";//get方式
+
+    public String loginUrl = "user/login";//post
+
+    public String registerUrl = "user/register";//post
 }
